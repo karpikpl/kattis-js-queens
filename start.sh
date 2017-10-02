1285c1d0c36b90f7f89a52d2a4bf3c42d0694e26
Piotr Karpala <pkarpala@infusion.com>
2017-10-01 22:23:49 -0400

    init script

---
+++ start.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+while getopts p:w: option
+do
+ case "${option}"
+ in
+ p) PROBLEM=${OPTARG};;
+ w) WORKDIR=${OPTARG};;
+ esac
+done
+
+if [ -z $PROBLEM ]
+  then
+    echo Please provide argument -p [problem name]
+    exit -1
+fi
+
+if [ -z $WORKDIR ]
+  then
+    echo Please provide argument -w [work directory]
+    exit -1
+fi
+
+cd $WORKDIR
+
+# clone solution repo
+git clone https://github.com/karpikpl/kattis-js-ecma2016.git $PROBLEM
+cd $PROBLEM
+
+# remove origin to not commit solution by accident
+git remote remove origin
+
+URL='https://open.kattis.com/problems/'$PROBLEM'/file/statement/samples.zip'
+
+echo downloding $URL
+mkdir -p datafiles
+
+# download sample data
+curl -o ./datafiles/samples.zip $URL
+unzip -o ./datafiles/samples.zip -d datafiles
+rm ./datafiles/samples.zip
+
+# commit sample data
+git add datafiles
+git commit -m 'adding data files'
+
+# install dependencies
+yarn
